624493c62d7f611f89705b89ea65a1633b939106
Jan Broniowski <jan.broniowski@contractors.roche.com>
2017-02-23 10:30:43 +0100

    weekofcode29, megaprimes, no prefix optimization

--- src/main/java/weekofcode29/MegaPrimeNumbers.java
+++ src/main/java/weekofcode29/MegaPrimeNumbers.java
@@ -3,7 +3,6 @@ package weekofcode29;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Scanner;
-import java.util.stream.Collectors;
 
 /**
  * Created by broniowj on 2017-02-23.
@@ -19,33 +18,13 @@ public class MegaPrimeNumbers {
 		long last = in.nextLong();
 		long counter = 0;
 
-		String prefix = prefixNumbers(first, last);
-
-		if (prefix.isEmpty() || areDigitsPrime(prefix)) {
-			for (long n = first; n <= last; n++) {
-				if (areDigitsPrime(String.valueOf(n)) && isPrime(n)) {
-						counter++;
-				}
+		for (long n = first; n <= last; n++) {
+			if (areDigitsPrime(String.valueOf(n)) && isPrime(n)) {
+				counter++;
 			}
 		}
+
 		System.out.println(counter);
-	}
-
-	static String prefixNumbers(Long f, Long l) {
-		if (f <= (l - f)) return "";
-
-		String prefix = "";
-
-		List<Character> fs = String.valueOf(f).chars().mapToObj(i -> (char) i).collect(Collectors.toList());
-		List<Character> ls = String.valueOf(l).chars().mapToObj(i -> (char) i).collect(Collectors.toList());
-
-		for (int i = 0; i < fs.size() && i < ls.size(); i++) {
-			if (fs.get(i).equals(ls.get(i))) {
-				prefix = prefix + fs.get(i);
-			}
-		}
-
-		return prefix;
 	}
 
 	static boolean areDigitsPrime(String s) {
